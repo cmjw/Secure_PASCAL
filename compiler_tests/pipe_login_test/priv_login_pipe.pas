30d671dac4c728a50d63047aaf18c7558480a5e9
cmjw <60287586+cmjw@users.noreply.github.com>
2024-06-15 19:51:06 -0500

    Fix pipe test

---
+++ compiler_tests/pipe_login_test/priv_login_pipe.pas
@@ -0,0 +1,30 @@
+program PrivilegedProgram;
+
+uses
+  SysUtils;
+
+const
+  PASSWORD = 'pascal123'; { Predefined password }
+
+var
+  inputPassword: string;
+  pipe: Text;
+
+begin
+  writeln('Privileged Program');
+  
+  // Read the password from the pipe
+  assign(pipe, 'pipe_to_privileged');
+  reset(pipe);
+  readln(pipe, inputPassword);
+  close(pipe);
+  
+  if inputPassword = PASSWORD then
+  begin
+    writeln('Access granted!');
+  end
+  else
+  begin
+    writeln('Access denied!');
+  end;
+end.
