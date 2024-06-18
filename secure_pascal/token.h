43c8f7ad87e3925b81be8a4e5857b8c019ed6752
cmjw <60287586+cmjw@users.noreply.github.com>
2024-06-18 11:34:35 -0500

    Rebuilding lexer/parser

--- secure_pascal/token.h
+++ secure_pascal/token.h
@@ -3,5 +3,15 @@
 
 /* Token data structure */
 typedef struct tok {
-
+    int tokentype;
+    int basicdt;
+    struct symtbr * symtype;
+    struct symtbr * symentry;
+    struct tok * operands;
+    struct tok * link;
+    union {
+        char tokenstring[16];
+        int which;
+        
+    }
 } *TOKEN;
