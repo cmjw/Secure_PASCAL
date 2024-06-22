77343eef88acc44bda88de90da9c8036eb8212e3
cmjw <60287586+cmjw@users.noreply.github.com>
2024-06-22 15:37:01 -0500

    Building parser

--- secure_pascal1/build.sh
+++ secure_pascal1/build.sh
@@ -4,12 +4,16 @@ GREEN="\e[33m"
 WHITE="\e[0m"
 
 echo -e "${GREEN}Building lexer (./lexer)${WHITE}"
-
+bison -d parser.y
 flex lexer.l
 
 make lexer
 
 echo -e "${GREEN}Building parser (./parser)${WHITE}"
+
+
+
+make parser
 
 echo "TBD"
 
