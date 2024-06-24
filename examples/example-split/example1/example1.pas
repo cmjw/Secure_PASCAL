76ca8341f65f4d148caeb686c620232ee3b0a78c
cmjw <60287586+cmjw@users.noreply.github.com>
2024-06-24 13:26:56 -0500

    Create example1.pas

---
+++ examples/example-split/example1/example1.pas
@@ -0,0 +1,15 @@
+program Example1(ouput); 
+{ must have output for now }
+
+var i : integer;
+{function getNum : integer;}
+
+begin
+    i := 1 { last statement has no end semicolon }
+end.
+
+function getNum : integer;
+
+begin  
+    result := 0;
+end.
