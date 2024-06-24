f758673592589babc224f845890bd43b5693db18
cmjw <60287586+cmjw@users.noreply.github.com>
2024-06-23 19:30:53 -0500

    Changes in preparation for building parser again

--- secure_pascal/printtoken.c
+++ secure_pascal/printtoken.c
@@ -5,7 +5,13 @@
 #include <ctype.h>
 #include "token.h"
 
-void printtoken(TOKEN tok);
+/* Allocate a new token record */
+TOKEN talloc() { 
+    TOKEN tok;
+    tok = (TOKEN) calloc(1,sizeof(struct tokn));
+    if ( tok != NULL ) return (tok);
+       else printf("talloc failed.");
+}
 
 static char* opprnt[] = {" ", "+", "-", "*", "/", ":=", "=", "<>", "<", "<=",
   ">=", ">",  "^", ".", "and", "or", "not", "div",
