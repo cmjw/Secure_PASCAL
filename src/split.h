ae3ed8d2754c926f68db519fab2c5aaae4947669
Charlotte White <charlotte.white@utexas.edu>
2024-06-30 20:18:04 -0500

    Update makefile

---
+++ src/split.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "token.h"
+#include "symtab.h"
+#include "lexer.h"
+#include "genasm.h"
+#include "codegen.h"
+#include "pprint.h"
+
+void split();
