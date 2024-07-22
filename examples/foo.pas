f1eb9b6a3011f13ae7c55f2958c9a7b65843b9c9
Charlotte White <charlotte.white@utexas.edu>
2024-07-21 22:05:19 -0500

    Update example

--- examples/foo.pas
+++ examples/foo.pas
@@ -5,10 +5,16 @@ var i : integer;
 
 begin
     i := 1; { last statement has no end semicolon }
-end.
+end { no dot }
 
-function foo(j : integer;): integer;
+function foo(jo : integer;): integer;
 var result : integer;
 begin
-    j := 0;
+    jo := 0;
+end;
+
+priv::function bar(jo : integer;): integer;
+var result : integer;
+begin
+    jo := 0;
 end;
