782ed74d96b24e4d9b7fb6e8c7dc909e48d9234d
Charlotte <charlotte.white@utexas.edu>
2024-08-21 15:50:48 -0500

    Update description

---
+++ examples/ex4.pas
@@ -0,0 +1,16 @@
+(* 
+ * Example program 4
+ * 
+ * Security policy level : STRICT or above
+ *
+ * Invalid function name
+*)
+
+program ex4(ouput); 
+
+var i : integer;
+
+begin
+    i := 1;
+    examplefunction(i);
+end.
